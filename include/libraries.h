6f5fb0dd95e7c5d31dcb374f3b8ab89d20c63674
Sqrdelta <Sqrdelta@github.noreply.com>
2021-05-12 20:32:19 +0200

    aggiornamento doxygen

--- include/libraries.h
+++ include/libraries.h
@@ -1,3 +1,16 @@
+/**
+ * @file libraries.h
+ * @author Fabio Spiriticchio - Sergio Mari
+ * @brief Libreria utilizzata come raccolta di costanti e librerie standard per lo sviluppo del 
+ *        sorgente.
+ * @version 0.1
+ * @date 2021-05-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
 #ifdef LIBRARIES_H_INCLUDED
 #define LIBRARIES_H_INCLUDED
 #define ID_LENGHT 30

899e47b13172dfd714333a89e6949b0d52a241e1
Sqrdelta <sqrdelta@github.noreply.com>
2021-05-13 11:10:14 +0200

    .

--- include/login.h
+++ include/login.h
@@ -1,3 +1,16 @@
+/**
+ * @file login.c
+ * @author Fabio Spiriticchio - Sergio Mari
+ * @brief Questa libreria viene utilizzata per implementare le seguenti funzioni:
+ *          1)Creazione del file utilizzato per memorizzare le credenziali.
+ *          2)La funzione che permetterà di creare l'account.
+ * @version 0.1
+ * @date 2021-05-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #ifndef LOGIN_H_INCLUDED
 #define LOGIN_H_INCLUDED
 
